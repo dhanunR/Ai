7495c712772ab9f54b601fa20b2e45053ef08dfe
Dhanun R Nair <109233637+dhanunR@users.noreply.github.com>
2023-10-19 21:44:25 +0530

    test.py

---
+++ test.py
@@ -0,0 +1,48 @@
+import subprocess
+
+# List of packages to install
+packages_to_install = [
+    "langchain",
+    "bitsandbytes",
+    "accelerate",
+    "xformers",
+    "einops",
+    "datasets",
+    "loralib",
+    "sentencepiece",
+    "pypdf",
+    "sentence_transformers",
+    "chromadb",
+    "openai",
+    "tiktoken",
+    # Add more package names here
+
+    # Import statements for additional modules
+    "import torch",
+    "import transformers",
+    "from transformers import AutoTokenizer, AutoModelForCausalLM",
+    "from transformers import pipeline",
+    "from langchain import HuggingFacePipeline",
+    "from langchain.chains import ConversationalRetrievalChain",
+    "from langchain.memory import ConversationBufferMemory",
+    "from langchain.embeddings.openai import OpenAIEmbeddings",
+    "from langchain.chat_models import ChatOpenAI",
+    "import os",
+    "import sys",
+    # Add more import statements here
+]
+
+# Function to install a package
+def install_package(package_name):
+    try:
+        subprocess.check_call(["pip", "install", package_name, "-q"])
+        print(f"Successfully installed {package_name}")
+    except subprocess.CalledProcessError as e:
+        print(f"Error installing {package_name}: {e}")
+
+# Install packages one by one
+for package in packages_to_install:
+    if not package.startswith("from ") and not package.startswith("import "):  # Skip import statements
+        install_package(package)
+
+# Keep adding more package names and import statements to the list and run the script to install them
