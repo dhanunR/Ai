a6e02b8aa2c2fbcc91e90017a7db19f7ba66a7aa
Dhanun R Nair <109233637+dhanunR@users.noreply.github.com>
2023-10-19 22:21:53 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,85 +1,30 @@
 import streamlit as st
-import streamlit.components.v1 as components
-import subprocess
-
-from PyPDF2 import PdfReader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-import google.generativeai as palm
-from langchain.embeddings import GooglePalmEmbeddings
-from langchain.llms import GooglePalm
-from langchain.vectorstores import FAISS
-from langchain.chains import ConversationalRetrievalChain
-from langchain.memory import ConversationBufferMemory
-
+from PyPDF2 import PdfFileReader
 import os
 
-# List of packages to install
-packages_to_install = [
-    "langchain",
-    "bitsandbytes",
-    "accelerate",
-    "xformers",
-    "einops",
-    "datasets",
-    "loralib",
-    "sentencepiece",
-    "pypdf",
-    "sentence_transformers",
-    "chromadb",
-    "openai",
-    "tiktoken",
-    "PyPDF2",
-    # Add more package names here
-
-    # Import statements for additional modules
-    "import torch",
-    "import transformers",
-    "from transformers import AutoTokenizer, AutoModelForCausalLM",
-    "from transformers import pipeline",
-    "from langchain import HuggingFacePipeline",
-    "from langchain.chains import ConversationalRetrievalChain",
-    "from langchain.memory import ConversationBufferMemory",
-    "from langchain.embeddings.openai import OpenAIEmbeddings",
-    "from langchain.chat_models import ChatOpenAI",
-    "import os",
-    "import sys",
-    # Add more import statements here
-]
-
-# Function to install a package
-def install_package(package_name):
-    try:
-        subprocess.check_call(["pip", "install", package_name, "-q"])
-        print(f"Successfully installed {package_name}")
-    except subprocess.CalledProcessError as e:
-        print(f"Error installing {package_name}: {e}")
-
-# Install packages one by one
-for package in packages_to_install:
-    if not package.startswith("from ") and not package.startswith("import "):  # Skip import statements
-        install_package(package)
-
+# Create a Streamlit app
 st.title("Quality Checker")
 st.write("This application will allow you to upload your dataset and run a quality check on it.")
 st.markdown("---")
 
-st.subheader("Upload your Documents")
-pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Process Button", accept_multiple_files=True)
-if st.button("Process"):
-    with st.spinner("Processing"):
-         pdf_reader = PdfReader.PdfFileReader(pdf_docs)
-        st.write(f"Total Pages: {pdf_reader.numPages}")
-for page_num in range(pdf_reader.numPages):
-    page = pdf_reader.getPage(page_num)
+# Function to process PDFs
+def process_pdf(pdf_file):
+    pdf_reader = PdfFileReader(pdf_file)
+    total_pages = pdf_reader.getNumPages()
+    st.write(f"Total Pages: {total_pages}")
+    for page_num in range(total_pages):
+        page = pdf_reader.getPage(page_num)
         page_text = page.extractText()
         st.subheader(f"Page {page_num + 1}")
         st.write(page_text)
 
-    # Close the PDF document
-    pdf_document.close()
-        #if pdf_docs.endswith(".pdf"):
-         #   loader=PyPDFLoader(pdf_docs)
-          #  document.extend(loader.load())
+# Upload PDF files
+pdf_docs = st.file_uploader("Upload your PDF Files", accept_multiple_files=True)
 
+if st.button("Process"):
+    if pdf_docs:
+        with st.spinner("Processing"):
+            for pdf_file in pdf_docs:
+                process_pdf(pdf_file)
 
-
+# Note: You don't need the package installation part in this code, as it's not directly related to the PDF processing.
