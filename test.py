517ddc4fbf7d03bc74ca6dfd07a427ede37edcb8
Dhanun R Nair <109233637+dhanunR@users.noreply.github.com>
2023-10-19 21:54:53 +0530

    Update test.py

--- test.py
+++ test.py
@@ -45,29 +45,6 @@ for package in packages_to_install:
     if not package.startswith("from ") and not package.startswith("import "):  # Skip import statements
         install_package(package)
 
-# Keep adding more package names and import statements to the list and run the script to install them
-st.set_page_config("Chat with Multiple PDFs")
-    st.header("Chat with Multiple PDF 💬")
-    user_question = st.text_input("Ask a Question from the PDF Files")
-    if "conversation" not in st.session_state:
-        st.session_state.conversation = None
-    if "chatHistory" not in st.session_state:
-        st.session_state.chatHistory = None
-    if user_question:
-        user_input(user_question)
-    with st.sidebar:
-        st.title("Settings")
-        st.subheader("Upload your Documents")
-        pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Process Button", accept_multiple_files=True)
-        if st.button("Process"):
-            with st.spinner("Processing"):
-                raw_text = get_pdf_text(pdf_docs)
-                text_chunks = get_text_chunks(raw_text)
-                vector_store = get_vector_store(text_chunks)
-                st.session_state.conversation = get_conversational_chain(vector_store)
-                st.success("Done")
-
-
-
-if __name__ == "__main__":
-    main()
+st.title("Quality Checker")
+st.write("This application will allow you to upload your dataset and run a quality check on it.")
+st.markdown("---")
