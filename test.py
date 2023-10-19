a91b1d5225d6ee044be2c31ae6be0d6e236c654b
Dhanun R Nair <109233637+dhanunR@users.noreply.github.com>
2023-10-19 22:35:29 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,26 +1,49 @@
 import streamlit as st
 from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.embeddings.openai import OpenAIEmbeddings  # Import OpenAIEmbeddings
 import subprocess
 import os
 
+# Set the OpenAI API key
+os.environ["OPENAI_API_KEY"] = "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"
+
 # Upgrade PyPDF2 to the latest version
 subprocess.call(["pip", "install", "PyPDF2", "--upgrade", "-q"])
-
 
 # Create a Streamlit app
 st.title("Quality Checker")
 st.write("This application will allow you to upload your dataset and run a quality check on it.")
 st.markdown("---")
 
+# Initialize Hugging Face embeddings
+embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')
+
 # Function to process PDFs
 def process_pdf(pdf_file):
     pdf_reader = PdfReader(pdf_file)
     total_pages = len(pdf_reader.pages)
     st.write(f"Total Pages: {total_pages}")
+    
+    # Initialize the CharacterTextSplitter
+    document_splitter = CharacterTextSplitter(separator='\n', chunk_size=500, chunk_overlap=100)
+    
     for page_num, page in enumerate(pdf_reader.pages, 1):
         page_text = page.extract_text()
-        st.subheader(f"Page {page_num}")
-        st.write(page_text)
+        
+        # Split the page text into chunks
+        document_chunks = document_splitter.split_documents([page_text])
+        
+        # Display each chunk separately
+        for chunk_num, chunk in enumerate(document_chunks, 1):
+            st.subheader(f"Page {page_num}, Chunk {chunk_num}")
+            st.write(chunk)
+            
+            # Generate embeddings for the chunk
+            chunk_embeddings = embeddings.embed([chunk])
+            st.write("Embeddings:")
+            st.write(chunk_embeddings)
 
 # Upload PDF files
 pdf_docs = st.file_uploader("Upload your PDF Files", accept_multiple_files=True)
