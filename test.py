3e04d8add5621a9e64fffdc5f2fcd886b99d03bc
Dhanun R Nair <109233637+dhanunR@users.noreply.github.com>
2023-10-20 11:36:40 +0530

    Update test.py

--- test.py
+++ test.py
@@ -23,6 +23,9 @@ required_packages = [
 for package in required_packages:
     subprocess.check_call(["pip", "install", package, "-q"])
 
+import streamlit as st
+import os
+
 # Retrieve the OpenAI API key from the environment variable
 api_key = os.environ.get("OPENAI_API_KEY")
 
@@ -34,23 +37,23 @@ else:
     st.write("This application allows you to upload and process PDF documents.")
     st.markdown("---")
 
-# Streamlit app setup
-#st.title("Quality Checker")
-#st.write("This application allows you to upload and process PDF documents.")
-#st.markdown("---")
+    # File Upload
+    uploaded_files = st.file_uploader("Upload PDF Files", type=["pdf"], accept_multiple_files=True)
 
-# Function to process PDFs
-def process_pdf(pdf_file):
-    pdf_reader = PdfFileReader(pdf_file)
-    total_pages = pdf_reader.getNumPages()
-    st.write(f"Total Pages: {total_pages}")
+    if st.button("Process PDF"):
+        if uploaded_files:
+            for pdf_file in uploaded_files:
+                with st.spinner(f"Processing {pdf_file.name}"):
+                    # Your PDF processing code goes here
+                    pdf_reader = PdfFileReader(pdf_file)
+                    total_pages = pdf_reader.getNumPages()
+                    st.write(f"Total Pages: {total_pages}")
     
-    # Initialize the CharacterTextSplitter
-    document_splitter = CharacterTextSplitter(separator='\n', chunk_size=500, chunk_overlap=100)
-    
-    for page_num in range(total_pages):
-        page = pdf_reader.getPage(page_num)
-        page_text = page.extract_text()
+                # Initialize the CharacterTextSplitter
+                document_splitter = CharacterTextSplitter(separator='\n', chunk_size=500, chunk_overlap=100)
+                for page_num in range(total_pages):
+                    page = pdf_reader.getPage(page_num)
+                    page_text = page.extract_text()
         
         # Split the page text into chunks
         document_chunks = document_splitter.split_documents([page_text])
@@ -60,11 +63,45 @@ def process_pdf(pdf_file):
             st.subheader(f"Page {page_num + 1}, Chunk {chunk_num}")
             st.write(chunk)
 
-# Upload PDF files
-pdf_docs = st.file_uploader("Upload your PDF Files", accept_multiple_files=True)
+                    # For example, you can process the PDF using your OpenAI API key
+                    # Make API requests using the `api_key` variable
 
-if st.button("Process"):
-    if pdf_docs:
-        with st.spinner("Processing"):
-            for pdf_file in pdf_docs:
-                process_pdf(pdf_file)
+        st.success(f"Processed: {pdf_file.name}")
+
+    # You can continue building your Streamlit application here
+
+# Streamlit app setup
+#st.title("Quality Checker")
+#st.write("This application allows you to upload and process PDF documents.")
+#st.markdown("---")
+
+# Function to process PDFs
+
+#def process_pdf(pdf_file):
+#    pdf_reader = PdfFileReader(pdf_file)
+#    total_pages = pdf_reader.getNumPages()
+#    st.write(f"Total Pages: {total_pages}")
+#    
+    # Initialize the CharacterTextSplitter
+#    document_splitter = CharacterTextSplitter(separator='\n', chunk_size=500, chunk_overlap=100)
+    
+#    for page_num in range(total_pages):
+#        page = pdf_reader.getPage(page_num)
+#        page_text = page.extract_text()
+#        
+        # Split the page text into chunks
+#        document_chunks = document_splitter.split_documents([page_text])
+        
+        # Display each chunk separately
+ #       for chunk_num, chunk in enumerate(document_chunks, 1):
+ #           st.subheader(f"Page {page_num + 1}, Chunk {chunk_num}")
+ #           st.write(chunk)
+
+# Upload PDF files
+#pdf_docs = st.file_uploader("Upload your PDF Files", accept_multiple_files=True)
+
+#if st.button("Process"):
+#    if pdf_docs:
+#        with st.spinner("Processing"):
+#            for pdf_file in pdf_docs:
+#                process_pdf(pdf_file)
