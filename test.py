df38dd23cde13100fb48ce43fa6c03a5b0106541
Dhanun R Nair <109233637+dhanunR@users.noreply.github.com>
2023-10-20 13:11:16 +0530

    Update test.py

--- test.py
+++ test.py
@@ -21,31 +21,31 @@ def split_text_into_chunks(text, chunk_size, chunk_overlap):
     return chunks
 
 # Function to process PDFs
-def process_pdf(pdf_file, chunk_size=500, chunk_overlap=100):
-    pdf_reader = PyPDF2.PdfReader(pdf_file)
-    total_pages = len(pdf_reader.pages)
-    st.write(f"Total Pages: {total_pages}")
-
+def process_pdf(pdf_files, chunk_size=500, chunk_overlap=100):
     total_chunks = 0
 
-    for page_num, page in enumerate(pdf_reader.pages, 1):
-        page_text = page.extract_text()
+    for pdf_file in pdf_files:
+        pdf_reader = PyPDF2.PdfReader(pdf_file)
+        total_pages = len(pdf_reader.pages)
+        st.write(f"Total Pages: {total_pages}")
 
-        # Split the page text into chunks
-        document_chunks = split_text_into_chunks(page_text, chunk_size, chunk_overlap)
-        total_chunks += len(document_chunks)
+        for page_num, page in enumerate(pdf_reader.pages, 1):
+            page_text = page.extract_text()
 
-        for chunk_num, chunk in enumerate(document_chunks, 1):
-            st.subheader(f"Page {page_num}, Chunk {chunk_num}")
-            st.write(chunk)
+            # Split the page text into chunks
+            document_chunks = split_text_into_chunks(page_text, chunk_size, chunk_overlap)
+            total_chunks += len(document_chunks)
+
+            for chunk_num, chunk in enumerate(document_chunks, 1):
+                st.subheader(f"Page {page_num}, Chunk {chunk_num}")
+                st.write(chunk)
 
     st.write(f"Total Chunks: {total_chunks}")
 
 # Upload PDF files
-pdf_docs = st.file_uploader("Upload your PDF Files", accept-multiple_files=True)
+pdf_docs = st.file_uploader("Upload your PDF Files", accept_multiple_files=True)
 
 if st.button("Process"):
     if pdf_docs:
         with st.spinner("Processing"):
-            for pdf_file in pdf_docs:
-                process_pdf(pdf_file)
+            process_pdf(pdf_docs)
