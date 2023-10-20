26c2521221496160a18d9ccc66827b6fea9b6d4e
Dhanun R Nair <109233637+dhanunR@users.noreply.github.com>
2023-10-20 11:42:55 +0530

    Update test.py

--- test.py
+++ test.py
@@ -18,33 +18,4 @@ else:
     # File Upload
     uploaded_files = st.file_uploader("Upload PDF Files", type=["pdf"], accept_multiple_files=True)
 
-    if st.button("Process PDF"):
-        if uploaded_files:
-            for pdf_file in uploaded_files:
-                with st.spinner(f"Processing {pdf_file.name}"):
-                    # Your PDF processing code goes here
-                    pdf_reader = PdfFileReader(pdf_file)
-                    total_pages = pdf_reader.getNumPages()
-                    st.write(f"Total Pages: {total_pages}")
-
-                    # Initialize the CharacterTextSplitter
-                    document_splitter = CharacterTextSplitter(separator='\n', chunk_size=500, chunk_overlap=100)
-
-                    for page_num in range(total_pages):
-                        page = pdf_reader.getPage(page_num)
-                        page_text = page.extract_text()
-
-                        # Split the page text into chunks
-                        document_chunks = document_splitter.split_documents([page_text])
-
-                        # Display each chunk separately
-                        for chunk_num, chunk in enumerate(document_chunks, 1):
-                            st.subheader(f"Page {page_num + 1}, Chunk {chunk_num}")
-                            st.write(chunk)
-
-                    # For example, you can process the PDF using your OpenAI API key
-                    # Make API requests using the `api_key` variable
-
-                st.success(f"Processed: {pdf_file.name}")
-
     # You can continue building your Streamlit application here
