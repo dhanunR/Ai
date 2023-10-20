fd6a46c5267469876d4179bf22a0e6751cde2772
Dhanun R Nair <109233637+dhanunR@users.noreply.github.com>
2023-10-20 11:16:36 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,64 @@
-import streamlit as st
-from PyPDF2 import PdfReader
-from langchain.text_splitter import CharacterTextSplitter
-from transformers import AutoTokenizer, AutoModel  
+#import streamlit as st
+#from PyPDF2 import PdfReader
+#from langchain.text_splitter import CharacterTextSplitter
+#from transformers import AutoTokenizer, AutoModel  
 #import AutoTokenizer from transformers
-import subprocess
-import os
+#import subprocess
+#import os
 
+# List of packages to install
+packages_to_install = [
+    "langchain",
+    "bitsandbytes",
+    "accelerate",
+    "xformers",
+    "einops",
+    "datasets",
+    "loralib",
+    "sentencepiece",
+    "pypdf",
+    "sentence_transformers",
+    "chromadb",
+    "openai",
+    "tiktoken",
+    "PyPDF2",
+    # Add more package names here
+
+     # Import statements for additional modules
+    "import torch",
+    "import streamlit as st",
+    "from PyPDF2 import PdfReader",
+    "import transformers",
+    "from transformers import AutoTokenizer, AutoModelForCausalLM",
+    "from transformers import pipeline",
+    "from transformers import AutoTokenizer, AutoModel",
+    "from langchain import HuggingFacePipeline",
+    "from langchain.chains import ConversationalRetrievalChain",
+    "from langchain.text_splitter import CharacterTextSplitter",
+    "from langchain.memory import ConversationBufferMemory",
+    "from langchain.embeddings.openai import OpenAIEmbeddings",
+    "from langchain.chat_models import ChatOpenAI",
+    "import subprocess",
+    "import os",
+
+    "import os",
+    "import sys",
+    # Add more import statements here
+]
+
+# Function to install a package
+def install_package(package_name):
+    try:
+        subprocess.check_call(["pip", "install", package_name, "-q"])
+        print(f"Successfully installed {package_name}")
+    except subprocess.CalledProcessError as e:
+        print(f"Error installing {package_name}: {e}")
+
+# Install packages one by one
+for package in packages_to_install:
+    if not package.startswith("from ") and not package.startswith("import "):  # Skip import statements
+        install_package(package)
+        
 # Install the transformers library
 subprocess.call(["pip", "install", "transformers", "-q"])
 
