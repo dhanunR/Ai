a4c0d547a599c76616d2a9d25e43b7f34d001eb3
Dhanun R Nair <109233637+dhanunR@users.noreply.github.com>
2023-10-20 11:20:56 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,12 @@
-#import streamlit as st
-#from PyPDF2 import PdfReader
-#from langchain.text_splitter import CharacterTextSplitter
-#from transformers import AutoTokenizer, AutoModel  
-#import AutoTokenizer from transformers
-#import subprocess
-#import os
+import streamlit as st
+from PyPDF2 import PdfFileReader, PdfFileReader
+from langchain.text_splitter import CharacterTextSplitter
+from transformers import AutoTokenizer, AutoModel
+import subprocess
+import os
 
-# List of packages to install
-packages_to_install = [
+# Install necessary packages
+required_packages = [
     "langchain",
     "bitsandbytes",
     "accelerate",
@@ -16,76 +15,43 @@ packages_to_install = [
     "datasets",
     "loralib",
     "sentencepiece",
-    "pypdf",
-    "sentence_transformers",
+    "pypdf2",
     "chromadb",
     "openai",
     "tiktoken",
-    "PyPDF2",
-    # Add more package names here
-
-     # Import statements for additional modules
-    "import torch",
-    "import streamlit as st",
-    "from PyPDF2 import PdfReader",
-    "import transformers",
-    "from transformers import AutoTokenizer, AutoModelForCausalLM",
-    "from transformers import pipeline",
-    "from transformers import AutoTokenizer, AutoModel",
-    "from langchain import HuggingFacePipeline",
-    "from langchain.chains import ConversationalRetrievalChain",
-    "from langchain.text_splitter import CharacterTextSplitter",
-    "from langchain.memory import ConversationBufferMemory",
-    "from langchain.embeddings.openai import OpenAIEmbeddings",
-    "from langchain.chat_models import ChatOpenAI",
-    "import subprocess",
-    "import os",
-    "import sys",
-    # Add more import statements here
+    "sentence_transformers",
 ]
+for package in required_packages:
+    subprocess.check_call(["pip", "install", package, "-q"])
 
-# Function to install a package
-def install_package(package_name):
-    try:
-        subprocess.check_call(["pip", "install", package_name, "-q"])
-        print(f"Successfully installed {package_name}")
-    except subprocess.CalledProcessError as e:
-        print(f"Error installing {package_name}: {e}")
+# Install or upgrade specific packages
+subprocess.check_call(["pip", "install", "transformers", "--upgrade", "-q"])
+subprocess.check_call(["pip", "install", "PyPDF2", "--upgrade", "-q"])
 
-# Install packages one by one
-for package in packages_to_install:
-    if not package.startswith("from ") and not package.startswith("import "):  # Skip import statements
-        install_package(package)
-        
-# Install the transformers library
-subprocess.call(["pip", "install", "transformers", "-q"])
-
-# Set the OpenAI API key
+# Set OpenAI API key
 os.environ["OPENAI_API_KEY"] = "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"
 
-# Upgrade PyPDF2 to the latest version
-subprocess.call(["pip", "install", "PyPDF2", "--upgrade", "-q"])
-
-# Create a Streamlit app
+# Streamlit app setup
 st.title("Quality Checker")
-st.write("This application will allow you to upload your dataset and run a quality check on it.")
+st.write("This application allows you to upload and process PDF documents.")
 st.markdown("---")
 
-# Load a pre-trained model and tokenizer from Hugging Face
+# Load pre-trained model and tokenizer
 model_name = 'sentence-transformers/all-MiniLM-L6-v2'
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModel.from_pretrained(model_name)
 
 # Function to process PDFs
 def process_pdf(pdf_file):
-    pdf_reader = PdfReader(pdf_file)
-    total_pages = len(pdf_reader.pages)
+    pdf_reader = PdfFileReader(pdf_file)
+    total_pages = pdf_reader.getNumPages()
     st.write(f"Total Pages: {total_pages}")
     
     # Initialize the CharacterTextSplitter
     document_splitter = CharacterTextSplitter(separator='\n', chunk_size=500, chunk_overlap=100)
     
-    for page_num, page in enumerate(pdf_reader.pages, 1):
+    for page_num in range(total_pages):
+        page = pdf_reader.getPage(page_num)
         page_text = page.extract_text()
         
         # Split the page text into chunks
@@ -93,7 +59,7 @@ def process_pdf(pdf_file):
         
         # Display each chunk separately
         for chunk_num, chunk in enumerate(document_chunks, 1):
-            st.subheader(f"Page {page_num}, Chunk {chunk_num}")
+            st.subheader(f"Page {page_num + 1}, Chunk {chunk_num}")
             st.write(chunk)
             
             # Tokenize and generate embeddings for the chunk
