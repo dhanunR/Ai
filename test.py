f021553b72a7193abfad5525bc6500d623f82fd1
Dhanun R Nair <109233637+dhanunR@users.noreply.github.com>
2023-10-20 11:22:35 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
 import streamlit as st
-from PyPDF2 import PdfFileReader, PdfFileReader
+from PyPDF2 import PdfFileReader
 from langchain.text_splitter import CharacterTextSplitter
-from transformers import AutoTokenizer, AutoModel
 import subprocess
 import os
 
@@ -16,17 +15,13 @@ required_packages = [
     "loralib",
     "sentencepiece",
     "pypdf2",
+    "sentence_transformers",
     "chromadb",
     "openai",
     "tiktoken",
-    "sentence_transformers",
 ]
 for package in required_packages:
     subprocess.check_call(["pip", "install", package, "-q"])
-
-# Install or upgrade specific packages
-subprocess.check_call(["pip", "install", "transformers", "--upgrade", "-q"])
-subprocess.check_call(["pip", "install", "PyPDF2", "--upgrade", "-q"])
 
 # Set OpenAI API key
 os.environ["OPENAI_API_KEY"] = "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"
@@ -35,11 +30,6 @@ os.environ["OPENAI_API_KEY"] = "sk-VWxYz1ghXSQWexjmf0TqT3BlbkFJj7JaYJY7PtQFVd6SS
 st.title("Quality Checker")
 st.write("This application allows you to upload and process PDF documents.")
 st.markdown("---")
-
-# Load pre-trained model and tokenizer
-model_name = 'sentence-transformers/all-MiniLM-L6-v2'
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModel.from_pretrained(model_name)
 
 # Function to process PDFs
 def process_pdf(pdf_file):
@@ -61,14 +51,6 @@ def process_pdf(pdf_file):
         for chunk_num, chunk in enumerate(document_chunks, 1):
             st.subheader(f"Page {page_num + 1}, Chunk {chunk_num}")
             st.write(chunk)
-            
-            # Tokenize and generate embeddings for the chunk
-            inputs = tokenizer(chunk, return_tensors="pt", padding=True, truncation=True)
-            with st.spinner("Generating Embeddings"):
-                outputs = model(**inputs)
-            chunk_embeddings = outputs.last_hidden_state.mean(dim=1).squeeze()
-            st.write("Embeddings:")
-            st.write(chunk_embeddings)
 
 # Upload PDF files
 pdf_docs = st.file_uploader("Upload your PDF Files", accept_multiple_files=True)
