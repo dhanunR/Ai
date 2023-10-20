51e0c4c8bf85491617c083895778060ef24ed029
Dhanun R Nair <109233637+dhanunR@users.noreply.github.com>
2023-10-20 12:20:19 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,26 +1,16 @@
 import streamlit as st
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
-import sentence_transformers  # Import sentence_transformers
 import subprocess
 import os
 
-# Set the OpenAI API key
-openai_api_key = os.getenv("OPENAI_API_KEY")
-
 # Upgrade PyPDF2 to the latest version
 subprocess.call(["pip", "install", "PyPDF2", "--upgrade", "-q"])
-
-# Install sentence-transformers package
-subprocess.call(["pip", "install", "sentence-transformers", "-q"])
 
 # Create a Streamlit app
 st.title("Quality Checker")
 st.write("This application will allow you to upload your dataset and run a quality check on it.")
 st.markdown("---")
-
-# Load a sentence-transformers model
-embedder = sentence_transformers.SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
 
 # Function to process PDFs
 def process_pdf(pdf_file):
@@ -35,17 +25,12 @@ def process_pdf(pdf_file):
         page_text = page.extract_text()
         
         # Split the page text into chunks
-        document_chunks = document_splitter.split_documents([page_text])
+        document_chunks = document_splitter.split_documents(page_text)
         
         # Display each chunk separately
         for chunk_num, chunk in enumerate(document_chunks, 1):
             st.subheader(f"Page {page_num}, Chunk {chunk_num}")
             st.write(chunk)
-            
-            # Generate embeddings for the chunk
-            chunk_embeddings = embedder.encode([chunk], convert_to_tensor=True)
-            st.write("Embeddings:")
-            st.write(chunk_embeddings)
 
 # Upload PDF files
 pdf_docs = st.file_uploader("Upload your PDF Files", accept_multiple_files=True)
