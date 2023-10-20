8b9d730e8cbd201c64c3330f8f49a090c656f0f6
Dhanun R Nair <109233637+dhanunR@users.noreply.github.com>
2023-10-20 11:57:17 +0530

    Update test.py

--- test.py
+++ test.py
@@ -4,16 +4,19 @@ from langchain.text_splitter import CharacterTextSplitter
 import subprocess
 import os
 
-# Retrieve the OpenAI API key from the environment variable
-api_key = os.environ.get("OPENAI_API_KEY")
+# Access the OpenAI API key from the environment variable
+openai_api_key = os.environ.get("OPENAI_API_KEY")
+
+st.title("Quality Checker")
+st.write("This application allows you to upload and process PDF documents.")
+st.markdown("---")
 
 # Check if the API key is available
-if api_key is None:
-    st.warning("API key not found. Please set the OPENAI_API_KEY environment variable.")
+if openai_api_key:
+    st.write(f"OpenAI API Key: {openai_api_key}")
+    st.info("API key is available and can be used for OpenAI requests.")
 else:
-    st.title("Quality Checker")
-    st.write("This application allows you to upload and process PDF documents.")
-    st.markdown("---")
+    st.warning("OpenAI API Key not found. Please set it as a GitHub secret.")
 
     # File Upload
     uploaded_files = st.file_uploader("Upload PDF Files", type=["pdf"], accept_multiple_files=True)
